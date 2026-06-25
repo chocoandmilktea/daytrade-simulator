09e980a93d63d594bee5e1ea11c91e9bf7ea971e
chocoandmilktea <choco.and.milktea.gj@gmail.com>
2026-06-25 16:33:28 +0900

    Delete api/test-jquants.js

--- api/test-jquants.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const apiKey = process.env.JQUANTS_API_KEY;
-  const url = "https://api.jquants.com/v2/equities/bars/minute?code=72030&date=20260624";
-  
-  const r = await fetch(url, { headers: { "x-api-key": apiKey } });
-  const json = await r.json();
-  
-  return res.status(r.status).json(json);
-}
